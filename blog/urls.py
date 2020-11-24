4f406774d47eed7606e57c73ef61e55f141b0a90
hadirga <halexandergc@gmail.com>
2020-11-24 19:01:20 -0300

    initial commit

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import BlogListView, BlogDetailView
+
+
+urlpatterns = [
+    path('', BlogListView.as_view(), name='home'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+]

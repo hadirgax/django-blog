159095db8bdaa68b6a59e1f95122479f899ff2fd
hadirga <halexandergc@gmail.com>
2020-12-02 22:59:51 -0300

    heroku config files and updates

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'odbg$u*h3hm-54+d%)*y$$19#&7litwv-@4(@imgf-usha1=1#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

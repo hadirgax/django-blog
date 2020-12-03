1a0ba8de3baa2c6abd8dd24573b518cc8a329cae
hadirga <halexandergc@gmail.com>
2020-12-02 23:16:48 -0300

    whitenoise update

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'whitenoise.runserver_nonstatic',  # new
+    'whitenoise.runserver_nostatic', # new!
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',  # new
     'accounts.apps.AccountsConfig',  # new
